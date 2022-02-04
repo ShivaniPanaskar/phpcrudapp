16fecaf35cb6a26a4c7a7fd3cf3e0ed729d0bf80
ShivaniPanaskar <shivanipanaskar844@gmail.com>
2022-02-04 17:55:02 +0530

    update

--- phpcrud.sql
+++ phpcrud.sql
@@ -1,10 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2022 at 01:19 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpcrud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
 
 CREATE TABLE `tblusers` (
   `ID` int(10) NOT NULL,
@@ -16,16 +37,16 @@ CREATE TABLE `tblusers` (
   `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+--
+-- Dumping data for table `tblusers`
+--
 
 INSERT INTO `tblusers` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Address`, `CreationDate`) VALUES
-
-
-
-ALTER TABLE `tblusers`
-  ADD PRIMARY KEY (`ID`);
-
-ALTER TABLE `tblusers`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+(0, 'roshana', 'N', 7584965213, 'roshana@gmail.com', 'Banglore', '2022-02-04 10:30:54'),
+(0, 'shivani', 'p', 8446386344, 'shivanipanaskar844@gmail.com', 'Pune', '2022-02-04 11:36:56'),
+(0, 'sakshi', 'P', 2584796317, 'sakshi@gmail.com', 'Pune', '2022-02-04 11:37:37');
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
